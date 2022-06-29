3b801d9cd0c881d95ad98be7efb5d4a5c96c5c56
Sri BLAEVOET <lilypoetry@yahoo.com>
2022-06-29 16:51:37 +0200

    Compte: connection, déconnection, editer

--- src/Kernel.php
+++ src/Kernel.php
@@ -8,4 +8,13 @@ use Symfony\Component\HttpKernel\Kernel as BaseKernel;
 class Kernel extends BaseKernel
 {
     use MicroKernelTrait;
-}
+
+    // Nouvelle pour EasyAdmin
+    /* public function registerBundle()
+    {
+        $bundles = array(
+            // ...
+            new EasyCorp\Bundle\EasyAdminBundle\EasyAdminBundle(),
+        );
+    }*/
+} 
