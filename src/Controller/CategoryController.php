3bb9e27ab195707eb13b238a6cef272dd7112748
Sri BLAEVOET <lilypoetry@yahoo.com>
2022-07-06 12:15:13 +0200

    Category Formulaire

--- src/Controller/CategoryController.php
+++ src/Controller/CategoryController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use App\Entity\Category;
 use App\Form\CategoryFormType;
+use App\Repository\ArticleRepository;
 use App\Repository\CategoryRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\RedirectResponse;
@@ -13,15 +14,25 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class CategoryController extends AbstractController
 {
+    /** 
+     * Route pour afficher la liste des categories
+     */
     #[Route('/category', name: 'app_category')]
-    public function index(CategoryRepository $categoryRepository): Response
+    public function index(CategoryRepository $categoryRepository, ArticleRepository $articleRepository, Request $request): Response
     {
         $category = $categoryRepository->findAll();
+        $id = $request->query->get("id");
+        $articles = $articleRepository->findBy(['category' => $id]);
 
         return $this->render('category/index.html.twig', [
-            'categories' => $category
+            'categories' => $category,
+            'articles' => $articles
         ]);
     }
+
+    /** 
+     * Route pour afficher le formulaire d'ajoute la nouvelle category
+     */
     #[Route('/category/new', name: 'new_category')]
     public function new(Request $request, CategoryRepository $categoryRepository): Response
     {
@@ -41,6 +52,10 @@ class CategoryController extends AbstractController
             'form' => $form->createView()
         ]);
     }
+
+    /** 
+     * Route pour recupecer une category par id et editer
+     */
     #[Route('/category/edit/{id}', name: 'edit_category', requirements:['id' => '\d+'])]
     public function edit(Category $category, Request $request, CategoryRepository $categoryRepository): Response 
     {
@@ -59,6 +74,10 @@ class CategoryController extends AbstractController
             'form' => $form->createView() 
         ]);
     }
+
+    /** 
+     * Route pour supprimer une category
+     */
     #[Route('/category/delete/{id}', name: 'delete_category', requirements:['id' => '\d+'], methods: ['POST'])]
     public function delete(Category $category, Request $request, CategoryRepository $categoryRepository): RedirectResponse 
     {
--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,7 +2,7 @@
 
 namespace App\Controller;
 
-use App\Entity\Article;
+// use App\Entity\Article;
 use App\Repository\ArticleRepository;
 use App\Repository\CategoryRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -10,18 +10,53 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Knp\Component\Pager\PaginatorInterface;
-use Doctrine\Persistence\ManagerRegistry;
-use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+// use Doctrine\Persistence\ManagerRegistry;
+// use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 class HomeController extends AbstractController
 {
     #[Route('/', name: 'app_home')]
-    public function index(): Response
+    public function index(CategoryRepository $categoryRepository, Request $request, ArticleRepository $articleRepository, PaginatorInterface $paginatorInterface): Response
     {
-        return $this->render('home/index.html.twig');
+        $category = $categoryRepository->findAll();
+        $id = $request->query->get("id");
+        $articles = $articleRepository->findBy(['category' => $id]);
+
+        if($id)
+        {
+            $idcat = $categoryRepository->find($id);
+            if(!$idcat){
+                $this->addFlash('error'," L'id $id n'existe pas");
+                return $this->render('bundles/TwigBundle/Exception/error404.html.twig');
+
+               }
+               else{
+                
+                $articles = $paginatorInterface->paginate(
+                    $articleRepository->findBy(['category' => $id]),
+                    $request->query->getInt('page',1),6
+                );
+               }
+        }
+        
+        return $this->render('home/index.html.twig', [
+            'categories' => $category,
+            'articles' => $articles,
+        ]);
+        //return $this->render('home/index.html.twig');
+
+        /* $categories = $categoryRepository->findAll();
+
+        return $this->render('home/index.html.twig', [
+            'articles' => $articles,
+            'categories' => $categories,
+        ]); */
     }
 
-    #[Route('/search', name: 'handleSearch')]
+    /**
+     * La route pour afficher les résultats des recherches par le formulaire de recherche dans la navbar
+     */
+    #[Route('/search', name: 'search')]
     public function handleSearch(Request $request, ArticleRepository $articleRepository, PaginatorInterface $paginatorInterface, CategoryRepository $categoryRepository)
     {
         $query = $request->query->get('query');
@@ -44,43 +79,43 @@ class HomeController extends AbstractController
     /**
      * Contrôleur de la page affichant les résultats des recherches faites par le formulaire de recherche dans la navbar
      */
-    // #[Route('/recherche/', name: 'search')]
-    // public function search(Request $request, PaginatorInterface $paginator, ManagerRegistry $doctrine): Response
-    // {
+    /* #[Route('/recherche/', name: 'search')]
+    public function search(Request $request, PaginatorInterface $paginator, ManagerRegistry $doctrine): Response
+    {
 
-    //     // Récupération du numéro de la page demandée dans l'url (si il existe pas, 1 sera pris à la place)
-    //     $requestedPage = $request->query->getInt('page', 1);
+        // Récupération du numéro de la page demandée dans l'url (si il existe pas, 1 sera pris à la place)
+        $requestedPage = $request->query->getInt('page', 1);
 
-    //     // Si la page demandée est inférieur à 1, erreur 404
-    //     if ($requestedPage < 1) {
-    //         throw new NotFoundHttpException();
-    //     }
+        // Si la page demandée est inférieur à 1, erreur 404
+        if ($requestedPage < 1) {
+            throw new NotFoundHttpException();
+        }
 
-    //     // On récupère la recherche de l'utilisateur depuis l'url ($_GET['q'])
-    //     $search = $request->query->get('s', '');
+        // On récupère la recherche de l'utilisateur depuis l'url ($_GET['q'])
+        $search = $request->query->get('s', '');
 
-    //     // Récupération du manager général des entités
-    //     $em = $doctrine->getManager();
+        // Récupération du manager général des entités
+        $em = $doctrine->getManager();
 
-    //     // Création d'une requête permettant de récupérer les articles pour la page actuelle, dont le titre ou le contenu contient la recherche de l'utilisateur
-    //     $query = $em
-    //         ->createQuery('SELECT a FROM App\Entity\Article a WHERE a.title  LIKE :search OR a.description LIKE :search ORDER BY a.updatedAt DESC')
-    //         ->setParameters([
-    //             'search' => '%' . $search . '%',
-    //         ]);
+        // Création d'une requête permettant de récupérer les articles pour la page actuelle, dont le titre ou le contenu contient la recherche de l'utilisateur
+        $query = $em
+            ->createQuery('SELECT a FROM App\Entity\Article a WHERE a.title  LIKE :search OR a.description LIKE :search ORDER BY a.updatedAt DESC')
+            ->setParameters([
+                'search' => '%' . $search . '%',
+            ]);
 
-    //     // Récupération des articles
-    //     $articles = $paginator->paginate(
-    //         $query,
-    //         $requestedPage,
-    //         10
-    //     );
+        // Récupération des articles
+        $articles = $paginator->paginate(
+            $query,
+            $requestedPage,
+            10
+        );
 
-    //     // Appel de la vue en lui envoyant les articles à afficher
-    //     return $this->render('home/result.html.twig', [
-    //         'articles' => $articles,
-    //     ]);
-    // }
+        // Appel de la vue en lui envoyant les articles à afficher
+        return $this->render('home/result.html.twig', [
+            'articles' => $articles,
+        ]);
+    } */
 
 }
 
